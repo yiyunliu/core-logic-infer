f437b031a2c4b0f80591aa0b47af855bc122ac2a
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2020-05-17 15:10:23 -0400

    initial commit

---
+++ test/cmsc838_final/core_test.clj
@@ -0,0 +1,7 @@
+(ns cmsc838-final.core-test
+  (:require [clojure.test :refer :all]
+            [cmsc838-final.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
