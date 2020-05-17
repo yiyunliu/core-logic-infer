c098df05dfe75871ba23703e63c037642638f90e
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2020-05-17 15:16:41 -0400

    add dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject cmsc838-final "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A HM-style typechecker implemented using core.logic"
+  :url "https://github.com/yiyunliu/cmsc838-final"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.match "1.0.0"]
+                 [org.clojure/core.logic "1.0.0"]]
   :main ^:skip-aot cmsc838-final.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
