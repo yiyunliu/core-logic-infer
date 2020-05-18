a4e91340aaa6d224ab7acfe01868f2f5633b7038
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2020-05-18 02:44:40 -0400

    add typing rules (part of) and t-geno

--- src/cmsc838_final/core.clj
+++ src/cmsc838_final/core.clj
@@ -63,6 +63,7 @@
 
 ;; (run* [q] (termo term-illegal))
 
+(declare typeo)
 
 ;; syntax for our terms
 ;; keeps track of the # of lambdas
@@ -71,6 +72,7 @@
   ([_ ['bv i]] (lto i m-i)) ;; not necessary to check i is nat in this case
   ([_ ['fv i]])
   ([_ ['λ ?e]] (term-i-o (list 'S m-i) ?e))
+  ([_ ['λ ?A ?e]] (typeo ?A) (term-i-o (list 'S m-i) ?e))
   ([_ ['ap e0 e1]] (term-i-o m-i e0) (term-i-o m-i e1)))
 
 (defn termo [e]
@@ -79,7 +81,7 @@
 
 (defne type-i-o [m-i t]
   ([_ 'bool])
-  ([_ ['Λ ?t]] (type-i-o (list 'S m-i) ?t))
+  ([_ ['∏ ?t]] (type-i-o (list 'S m-i) ?t))
   ([_ [?t0 '-> ?t1]] (type-i-o m-i ?t0) (type-i-o m-i ?t1))
   ([_ ['bv ?i]] (lto ?i m-i))
   ([_ ['fv ?i]]))
@@ -90,13 +92,13 @@
 
 
 (def type-fst
-  '(Λ (Λ ((bv (S Z)) -> ((bv Z) -> (bv (S Z)))))))
+  '(∏ (∏ ((bv (S Z)) -> ((bv Z) -> (bv (S Z)))))))
 
 ;; (run* [q] (typeo type-fst))
 
 
 (def type-snd
-  '(Λ (Λ ((bv (S Z)) -> ((bv Z) -> (bv Z))))))
+  '(∏ (∏ ((bv (S Z)) -> ((bv Z) -> (bv Z))))))
 
 ;; (run* [q] (typeo type-snd))
 
@@ -119,6 +121,9 @@
   ([_ _ ['λ ?e] _] (fresh (?e-opened)
                      (== ['λ ?e-opened] t-opened)
                      (term-nocheck-openo (list 'S k) x ?e ?e-opened)))
+  ([_ _ ['λ ?A ?e] _] (fresh (?e-opened)
+                     (== ['λ ?A ?e-opened] t-opened)
+                     (term-nocheck-openo (list 'S k) x ?e ?e-opened)))
   ;; bug bug bug!!
   ([_ _ ['ap ?e0 ?e1] _] (fresh (?e0-opened ?e1-opened)
                        (== t-opened (list 'ap ?e0-opened ?e1-opened))
@@ -133,6 +138,9 @@
   ([_ _ ['fv ?x] ['bv k]] (!= ?x x))
   ([_ _ ['λ ?e] _] (fresh (?e-closed)
                      (== ['λ ?e-closed] t-closed)
+                     (term-nocheck-closeo (list 'S k) x ?e ?e-closed)))
+  ([_ _ ['λ ?A ?e] _] (fresh (?e-closed)
+                     (== ['λ ?A ?e-closed] t-closed)
                      (term-nocheck-closeo (list 'S k) x ?e ?e-closed)))
   ;; bug bug bug!!
   ([_ _ ['ap ?e0 ?e1] _] (fresh (?e0-closed ?e1-closed)
@@ -158,8 +166,8 @@
   ([_ _ ['bv k] ['fv x]])
   ([_ _ ['bv ?k] _] (!= ?k k))
   ([_ _ ['fv ?x] ['fv ?x]])
-  ([_ _ ['Λ ?e] _] (fresh (?e-opened)
-                     (== ['Λ ?e-opened] t-opened)
+  ([_ _ ['∏ ?e] _] (fresh (?e-opened)
+                     (== ['∏ ?e-opened] t-opened)
                      (type-nocheck-openo (list 'S k) x ?e ?e-opened)))
   ;; bug bug bug!!
   ([_ _ [?e0 '-> ?e1] _] (fresh (?e0-opened ?e1-opened)
@@ -171,10 +179,10 @@
   ;; open
   ([_ _ _ _] (boolo t) (== t-closed t))
   ([_ _ ['bv ?k] t])
-  ([_ _ ['fv x] ['bv k]])
-  ([_ _ ['fv ?x] ['bv k]] (!= ?x x))
-  ([_ _ ['Λ ?e] _] (fresh (?e-closed)
-                     (== ['Λ ?e-closed] t-closed)
+  ([_ x ['fv x] ['bv k]] )
+  ([_ _ ['fv ?x] t] (!= ?x x))
+  ([_ _ ['∏ ?e] _] (fresh (?e-closed)
+                     (== ['∏ ?e-closed] t-closed)
                      (type-nocheck-closeo (list 'S k) x ?e ?e-closed)))
   ;; bug bug bug!!
   ([_ _ [?e0 '-> ?e1] _] (fresh (?e0-closed ?e1-closed)
@@ -190,3 +198,159 @@
 ;; can't be defined in types of openo
 (defn type-closeo [x t t-closed]
   (type-nocheck-closeo 'Z x t t-closed))
+
+
+;; List (var,type)
+(defne typing-contexto [ctx]
+  ([[]])
+  ([[[x t] . ?ctx ]]
+   (typeo t)
+   (typing-contexto ?ctx)))
+
+;; List type
+(defne application-contexto [ctx]
+  ([[]])
+  ([[?t . ?ctx]] (typeo ?t) (application-contexto ?ctx)))
+
+;; this is what happens when you host your language on JVM
+(declare subtypingo)
+(declare application-subtypingo)
+
+;; assoc list lookup
+;; disequality constraint is unnecessary
+;; since we use the barendrecht convention
+(defn lookupo [x A ctx]
+  (membero [x A] ctx))
+
+(defne map-fsto [ctx ctxo]
+  ([[] []])
+  ([[[?a ?b] . ?ctx] _]
+   (fresh (?ctxo)
+     (conso ?a ?ctxo ctxo)
+     (map-fsto ?ctx ?ctxo))))
+
+(defne map-sndo [ctx ctxo]
+  ([[] []])
+  ([[[?a ?b] . ?ctx] _]
+   (fresh (?ctxo)
+     (conso ?b ?ctxo ctxo)
+     (map-sndo ?ctx ?ctxo))))
+
+(defne type-fvo [t tvs]
+  (['bool []])
+  ([['∏ ?t] _] (type-fvo ?t tvs))
+  ([[?t0 '-> ?t1] ?tvs]
+   (fresh [?tvs0 ?tvs1]
+     (type-fvo ?t0 ?tvs0)
+     (type-fvo ?t1 ?tvs1)
+     (appendo ?tvs0 ?tvs1 ?tvs)))
+  ([['bv _] []])
+  ([['fv ?i] [?i]]))
+
+(defne term-fvo [e fvs]
+  ([_ _] (boolo e) (== fvs []))
+  ([['bv i] []]) ;; not necessary to check i is nat in this case
+  ([['fv i] [i]])
+  ([['λ ?e] _] (term-fvo ?e fvs))
+  ([['λ _ ?e] _] (term-fvo ?e fvs))
+  ([['ap e0 e1] _] (fresh [fvs0 fvs1]
+                     (term-fvo e0 fvs0)
+                     (term-fvo e1 fvs1)
+                     (appendo fvs0 fvs1 fvs))))
+
+(defne rem-allo [x xs ys]
+  ([_ [] []])
+  ([x [x . ?xs] ?ys]
+   (rem-allo x ?xs ?ys))
+  ([x [?x . ?xs] [?x . ?ys]]
+   (!= x ?x)
+   (rem-allo x ?xs ?ys)))
+
+(defne differenceo [l0 l1 ld]
+  ([ _ [] l0])
+  ([ _ [?x . ?l1] ?l2]
+   (fresh [?l0]
+     (rem-allo ?x l0 ?l0)
+     (differenceo ?l0 ?l1 ?l2))))
+
+(defne ctx-fvo [ll lout]
+  ([[] []])
+  ([[[_ ?x] . ?l1] _]
+   (fresh [?fvs ?lout]
+     (type-fvo ?x ?fvs)
+     (appendo ?fvs ?lout lout)
+     (ctx-fvo ?l1 ?lout))))
+
+(defne close-with-fvarso [vars A A-generalized]
+  ([[] A A])
+  ([[?v . ?vars] _ _]
+   (fresh [B]
+     (type-closeo ?v A B)
+     (close-with-fvarso ?vars ['∏ B] A-generalized))))
+
+(def type-fst-fv
+  '((fv x) -> ((fv y) -> (fv x))))
+
+(defne dedupo [xs ys]
+  ([[] []])
+  ([[?x . ?xs] [?x . ?zs]]
+   (fresh [?ys]
+     (rem-allo ?x ?xs ?ys)
+     (dedupo ?ys ?zs))))
+
+(defn t-geno [ctx A A-generalized]
+  (fresh [A-fvars ctx-fvars A-ctx-diff diff-deduped]
+    (ctx-fvo ctx ctx-fvars)
+    (type-fvo A A-fvars)
+    (differenceo A-fvars ctx-fvars A-ctx-diff)
+    (dedupo A-ctx-diff diff-deduped)
+    (close-with-fvarso diff-deduped A A-generalized)))
+
+(defne typing-state-o [gen ctx actx e B]
+  ;; t-var
+  ([['fv x] _ [] _ _ ]
+   (fresh [A]
+     (lookupo x A ctx)
+     (application-subtypingo actx A B)))
+  
+  ;; t-int
+  ([_ _ _ _ 'bool]
+   (boolo e))
+  
+  ;; t-lam
+  ([_ ctx [?A .  ?actx] ['λ e] [?A '-> ?B]]
+   (fresh [x ?ctx]
+     (== x ['S gen])
+     (conso [x ?A] ctx ?ctx)
+     (typing-state-o x ?ctx actx e ?B)))
+
+
+  ;; t-lam2
+  ([_ ctx [] ['λ e] [?t '-> ?B]]
+   (fresh [x ?ctx]
+     (== x ['S gen])
+     (conso [x ?t] ctx ?ctx)
+     (typing-state-o x ?ctx actx e ?B)
+     (monotypeo ?t)))
+
+  ;; t-lamann1
+ ([_ ctx [] ['λ ?A e] [?A '-> ?B]]
+   (fresh [x ?ctx]
+     (== x ['S gen])
+     (conso [x ?A] ctx ?ctx)
+     (typing-state-o x ?ctx actx e ?B)))
+
+  ;; t-lamann2
+ ([_ ctx [?C . ?actx] ['λ ?A e] [?C '-> ?B]]
+   (fresh [x ?ctx]
+     (== x ['S gen])
+     (conso [x ?A] ctx ?ctx)
+     (subtypingo ?C ?A)     
+     (typing-state-o x ?ctx actx e ?B)))
+
+  ;; t-gen
+  ()
+  )
+
+(defn typingo [ctx actx e B]
+  (typing-state-o 'Z ctx actx e B))
