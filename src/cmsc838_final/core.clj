e90ae1f4c8080f369c69c4e9a82e7cbf033b6433
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2020-05-19 15:24:35 -0400

    index refactoring mostly done

--- src/cmsc838_final/core.clj
+++ src/cmsc838_final/core.clj
@@ -11,236 +11,57 @@
   [& args]
   (println "Hello, World!"))
 
-(defn nat->int [x]
-  (match [x]
-         ['Z] 0
-         [(['S y] :seq)] (+ 1 (nat->int y))))
-
-(defn int->nat [x]
-  (match [x]
-         [0] 'Z
-         :else (list 'S (int->nat (- x 1)))))
-
-(defne nato [x]
-  ([['S ?o]] (nato ?o))
-  (['Z]))
-
-(defne pluso [x y o]
-  (['Z y y])
-  ([['S ?x] _ _]
-   (fresh [?o]
-     (== (list 'S ?o) o)
-     (pluso ?x y ?o) )))
-
-(defn leqo [x y]
-  (fresh (z)
-    (pluso z x y)))
-
-(defn lto [x y]
-  (fresh (z)
-    (leqo (list 'S x) y)))
-
-(defne boolo [x]
-  ([true])
-  ([false]))
-
-(def term-id
-  '(λ (bv Z)))
-
-;; (run* [q] (termo term-id))
-
-(def term-fst
-  '(λ (λ (bv (S Z)))))
-
-;; (run* [q] (termo term-fst))
-
-(def term-snd
-  '(λ (λ (bv Z))))
-
-;; (run* [q] (termo term-snd))
-
-(def term-illegal
-  '(λ (λ (bv (S (S Z))))))
-
-;; (run* [q] (termo term-illegal))
-
 (declare typeo)
 
 ;; syntax for our terms
 ;; keeps track of the # of lambdas
-(defne term-i-o [m-i e]
-  ([_ ['fv i]])
-  ([_ ['bv i]] (lto i m-i)) ;; not necessary to check i is nat in this case
-  ([_ ['λ ?e]] (term-i-o (list 'S m-i) ?e))
-  ([_ ['λ ?A ?e]] (typeo ?A) (term-i-o (list 'S m-i) ?e))
-  ([_ ['ap e0 e1]] (term-i-o m-i e0) (term-i-o m-i e1))
-  ([_ _] (boolo e)))
+(defne termo [e]
+  ([['v i]] ) ;; not necessary to check i is nat in this case
+  ([['λ ?e]]
+   (fresh [?body]
+     (nom/fresh [x]
+       (== ?e (nom/tie x ?body))
+       (termo ?body))))
+  ([['λ ?A ?e]] (typeo ?A)
+   (typeo ?A)
+   (fresh [?body]
+     (nom/fresh [x]
+       (== ?e (nom/tie x ?body))
+       (termo ?body))))
+  ([['ap e0 e1]]
+   (termo e0)
+   (termo e1))
+  (['true])
+  (['false]))
 
-(defn termo [e]
-  (term-i-o 'Z e))
+;; fst
+;; (run 1 [q] (nom/fresh [x y] (termo ['λ (nom/tie x ['λ (nom/tie y ['v x])])])))
 
-(defne closed-term-i-o [m-i e]
-  ([_ ['bv i]] (lto i m-i)) ;; not necessary to check i is nat in this case
-  ([_ ['λ ?e]] (closed-term-i-o (list 'S m-i) ?e))
-  ([_ ['λ ?A ?e]] (typeo ?A) (closed-term-i-o (list 'S m-i) ?e))
-  ([_ ['ap e0 e1]] (closed-term-i-o m-i e0) (closed-term-i-o m-i e1))
-  ([_ _] (boolo e)))
+;; snd
+;; (run 1 [q] (nom/fresh [x y] (termo ['λ (nom/tie x ['λ (nom/tie y ['v y])])])))
 
-(defn closed-termo [e]
-  (closed-term-i-o 'Z e))
-
-
-(defne type-i-o [m-i t]
-  ([_ ['fv ?i]])
-  ([_ [?t0 '-> ?t1]] (type-i-o m-i ?t0) (type-i-o m-i ?t1))
-  ([_ ['∏ ?t]] (type-i-o (list 'S m-i) ?t))
-  ([_ ['bv ?i]] (lto ?i m-i))
-  ([_ 'bool]))
-
-
-(defn typeo [t]
-  (type-i-o 'Z t))
-
-(defne closed-type-i-o [m-i t]
-  ([_ [?t0 '-> ?t1]] (closed-type-i-o m-i ?t0) (closed-type-i-o m-i ?t1))
-  ([_ ['∏ ?t]] (closed-type-i-o (list 'S m-i) ?t))
-  ([_ ['bv ?i]] (lto ?i m-i))
-  ([_ 'bool]))
+;; id
+;; (run 1 [q] (nom/fresh [y] (termo ['λ (nom/tie y ['v y])])))
 
 
-(defn closed-typeo [t]
-  (closed-type-i-o 'Z t))
+(defne typeo [t]
+  ([['v ?i]])
+  ([[?t0 '-> ?t1]] (typeo ?t0) (typeo ?t1))
+  ([['∏ ?t]]
+   (fresh [?body]
+     (nom/fresh [x]
+       (== ?t (nom/tie x ?body))
+       (typeo ?body))))
+  (['bool]))
 
-
-(def type-fst
-  '(∏ (∏ ((bv (S Z)) -> ((bv Z) -> (bv (S Z)))))))
-
-(def type-fst-v2
-  '[∏ [∏ ([bv Z] -> ([bv (S Z)] -> [bv Z]))]])
-
-;; (run* [q] (typeo type-fst))
-
-
-(def type-snd
-  '(∏ (∏ ((bv (S Z)) -> ((bv Z) -> (bv Z))))))
-
-;; (run* [q] (typeo type-snd))
+;; fst
+;; (run 1 [q] (nom/fresh [x y] (typeo ['∏ (nom/tie x ['∏ (nom/tie y [['v x] '-> [['v y] '-> ['v x]]])])])))
 
 
 (defne monotypeo [t]
-  ([['fv ?i]])
+  ([['v ?i]])
   ([[?t0 '-> ?t1]] (monotypeo ?t0) (monotypeo ?t1))  
   (['bool]))
-
-
-
-
-;; precondition: t is a term
-(defne term-nocheck-openo [k x t t-opened]
-  ;; open
-  ([_ _ ['fv ?x] ['fv ?x]])
-  ([_ _ ['bv k] ['fv x]])
-  ([_ _ ['bv ?k] t] (!= ?k k))
-  ([_ _ ['λ ?e] _] (fresh (?e-opened)
-                     (== ['λ ?e-opened] t-opened)
-                     (term-nocheck-openo (list 'S k) x ?e ?e-opened)))
-  ([_ _ ['λ ?A ?e] _] (fresh (?e-opened)
-                        (== ['λ ?A ?e-opened] t-opened)
-                        (term-nocheck-openo (list 'S k) x ?e ?e-opened)))
-  ([_ _ ['ap ?e0 ?e1] _] (fresh (?e0-opened ?e1-opened)
-                           (== t-opened (list 'ap ?e0-opened ?e1-opened))
-                           (term-nocheck-openo k x ?e0 ?e0-opened)
-                           (term-nocheck-openo k x ?e1 ?e1-opened)))
-  ;; bug bug bug!!
-  ([_ _ _ _] (boolo t) (== t-opened t)))
-
-(defne term-nocheck-closeo [k x t t-closed]
-  ;; open
-  ([_ _ ['fv x] ['bv k]])
-  ([_ _ ['fv ?x] ['bv k]] (!= ?x x))
-  ([_ _ ['bv _] t])
-  ([_ _ ['λ ?e] _] (fresh (?e-closed)
-                     (== ['λ ?e-closed] t-closed)
-                     (term-nocheck-closeo ['S k] x ?e ?e-closed)))
-  ([_ _ ['λ ?A ?e] _] (fresh (?e-closed)
-                        (== ['λ ?A ?e-closed] t-closed)
-                        (term-nocheck-closeo ['S k] x ?e ?e-closed)))
-  ([_ _ ['ap ?e0 ?e1] _] (fresh (?e0-closed ?e1-closed)
-                           (== t-closed (list 'ap ?e0-closed ?e1-closed))
-                           (term-nocheck-closeo k x ?e0 ?e0-closed)
-                           (term-nocheck-closeo k x ?e1 ?e1-closed)))
-  ;; bug bug bug!!
-  ([_ _ _ _] (boolo t) (== t-closed t)))
-
-
-(defn term-openo [x t t-opened]
-  (term-nocheck-openo 'Z x t t-opened))
-
-
-;; can't be defined in terms of openo
-(defn term-closeo [x t t-closed]
-  (term-nocheck-closeo 'Z x t t-closed))
-
-;; (run* [q] (term-closeo 'x  '(λ (fv x)) q))
-
-
-(defne type-nocheck-openo [k x t t-opened]
-  ;; open
-  ([_ _ ['fv ?x] ['fv ?x]])
-  ([_ _ ['bv k] ['fv x]])
-  ([_ _ ['bv ?k] t] (!= ?k k))
-  ([_ _ ['∏ ?e] _] (fresh (?e-opened)
-                     (== ['∏ ?e-opened] t-opened)
-                     (type-nocheck-openo (list 'S k) x ?e ?e-opened)))
-  ([_ _ [?e0 '-> ?e1] _] (fresh (?e0-opened ?e1-opened)
-                           (== t-opened (list ?e0-opened '-> ?e1-opened))
-                           (type-nocheck-openo k x ?e0 ?e0-opened)
-                           (type-nocheck-openo k x ?e1 ?e1-opened)))
-  ;; bug bug bug!!
-  ([_ _ 'bool 'bool]))
-
-
-(defne type-nocheck-sub-openo [k x t t-opened]
-  ;; open
-  ([_ _ ['bv k] x])
-  ([_ _ ['bv ?k] t] (!= ?k k))
-  ([_ _ ['fv ?x] ['fv ?x]])
-  ([_ _ ['∏ ?e] _] (fresh (?e-opened)
-                     (== ['∏ ?e-opened] t-opened)
-                     (type-nocheck-sub-openo (list 'S k) x ?e ?e-opened)))
-  ([_ _ [?e0 '-> ?e1] _] (fresh (?e0-opened ?e1-opened)
-                           (== t-opened (list ?e0-opened '-> ?e1-opened))
-                           (type-nocheck-sub-openo k x ?e0 ?e0-opened)
-                           (type-nocheck-sub-openo k x ?e1 ?e1-opened)))
-  ;; bug bug bug!!
-  ([_ _ 'bool 'bool]))
-
-(defn type-sub-openo [x t t-opened]
-  (type-nocheck-sub-openo 'Z x t t-opened))
-
-(defne type-nocheck-closeo [k x t t-closed]
-  ;; open
-  ([_ x ['fv x] ['bv k]] )
-  ([_ _ ['bv ?k] t])
-  ([_ _ ['fv ?x] t] (!= ?x x))
-  ([_ _ ['∏ ?e] _] (fresh (?e-closed)
-                     (== ['∏ ?e-closed] t-closed)
-                     (type-nocheck-closeo (list 'S k) x ?e ?e-closed)))
-  ([_ _ [?e0 '-> ?e1] _] (fresh (?e0-closed ?e1-closed)
-                           (== t-closed (list ?e0-closed '-> ?e1-closed))
-                           (type-nocheck-closeo k x ?e0 ?e0-closed)
-                           (type-nocheck-closeo k x ?e1 ?e1-closed)))
-  ;; bug bug bug!!
-  ([_ _ _ _] (boolo t) (== t-closed t)))
-
-
-(defn type-openo [x t t-opened]
-  (type-nocheck-openo 'Z x t t-opened))
-
-
-;; can't be defined in types of openo
-(defn type-closeo [x t t-closed]
-  (type-nocheck-closeo 'Z x t t-closed))
 
 
 ;; List (var,type)
@@ -249,6 +70,12 @@
   ([[[x t] . ?ctx ]]
    (typeo t)
    (typing-contexto ?ctx)))
+
+;; context lookup
+;; disequality constraint is unnecessary
+;; since nom ensures barendrecht convention
+(defn lookupo [x A ctx]
+  (membero [x A] ctx))
 
 ;; List type
 (defne application-contexto [ctx]
@@ -259,11 +86,7 @@
 (declare subtypingo)
 (declare application-subtypingo)
 
-;; assoc list lookup
-;; disequality constraint is unnecessary
-;; since we use the barendrecht convention
-(defn lookupo [x A ctx]
-  (membero [x A] ctx))
+
 
 (defne map-fsto [ctx ctxo]
   ([[] []])
@@ -285,42 +108,28 @@
      (conso ?b ?ctxo ctxo)
      (map-sndo ?ctx ?ctxo))))
 
+;; i think it's more efficient to construct a list
+;; and check x \in list than checking x \in t
+;; everytime
 (defne type-fvo [t tvs]
-  ([['fv ?i] [?i]])
-  ([['∏ ?t] _] (type-fvo ?t tvs))
+  ([['v ?i] [?i]])
+  ([['∏ ?t]]
+   (fresh [?body]
+     (nom/fresh [x]
+       (== ?t (nom/tie x ?body))
+       (type-fvo ?body tvs))))
   ([[?t0 '-> ?t1] ?tvs]
    (fresh [?tvs0 ?tvs1]
      (type-fvo ?t0 ?tvs0)
      (type-fvo ?t1 ?tvs1)
      (appendo ?tvs0 ?tvs1 ?tvs)))
-  ([['bv _] []])
   (['bool []]))
 
-(defne term-fvo [e fvs]
-  ([['bv i] []])
-  ([['fv i] [i]]) ;; not necessary to check i is nat in this case
-  ([['λ ?e] _] (term-fvo ?e fvs))
-  ([['λ _ ?e] _] (term-fvo ?e fvs))
-  ([['ap e0 e1] _] (fresh [fvs0 fvs1]
-                     (term-fvo e0 fvs0)
-                     (term-fvo e1 fvs1)
-                     (appendo fvs0 fvs1 fvs)))
-  ([_ _] (boolo e) (== fvs [])))
-
-(defne rem-allo [x xs ys]
-  ([_ [] []])
-  ([x [x . ?xs] ?ys]
-   (rem-allo x ?xs ?ys))
-  ([x [?x . ?xs] [?x . ?ys]]
-   (!= x ?x)
-   (rem-allo x ?xs ?ys)))
-
-(defne differenceo [l0 l1 ld]
-  ([ _ [] l0])
-  ([ _ [?x . ?l1] ?l2]
-   (fresh [?l0]
-     (rem-allo ?x l0 ?l0)
-     (differenceo ?l0 ?l1 ?l2))))
+;; name capture is avoided as one would expect
+;; (run 1 [q] (nom/fresh [x y]
+;;              (fresh [tvs]
+;;                (type-fvo ['∏ (nom/tie x ['∏ (nom/tie y [['v x] '-> [['v y] '-> ['v x]]])])] tvs)
+;;                (membero x tvs))))
 
 (defne ctx-fvo [ll lout]
   ([[] []])
@@ -330,32 +139,45 @@
      (appendo ?fvs ?lout lout)
      (ctx-fvo ?l1 ?lout))))
 
-(defne close-with-fvarso [vars A A-generalized]
-  ([[] A A])
-  ([[?v . ?vars] _ _]
-   (fresh [B]
-     (type-closeo ?v A B)
-     (close-with-fvarso ?vars ['∏ B] A-generalized))))
-
-(def type-fst-fv
-  '((fv x) -> ((fv y) -> (fv x))))
-
-(defne dedupo [xs ys]
-  ([[] []])
-  ([[?x . ?xs] [?x . ?zs]]
-   (fresh [?ys]
-     (rem-allo ?x ?xs ?ys)
-     (dedupo ?ys ?zs))))
+(defne bind-with-fvso [A-fvs ctx-fvs A A-out]
+  ([ [] _ A A])
+  ([ [x . xs] _ A _]
+   (nom/hash x ctx-fvs)
+   (nom/hash x xs)
+   (bind-with-fvso xs ctx-fvs ['∏ (nom/tie x A)] A-out))
+  ([ [x . xs] _ A _]
+   (conde
+    ((membero x xs))
+    ((membero x ctx-fvs)))
+   (bind-with-fvso xs ctx-fvs A A-out)))
 
 (defn t-geno [ctx A A-generalized]
-  (fresh [A-fvars ctx-fvars A-ctx-diff diff-deduped]
-    (ctx-fvo ctx ctx-fvars)
-    (type-fvo A A-fvars)
-    (differenceo A-fvars ctx-fvars A-ctx-diff)
-    (dedupo A-ctx-diff diff-deduped)
-    (close-with-fvarso diff-deduped A A-generalized)))
+  (fresh [A-fvs ctx-fvs]
+    (ctx-fvo ctx ctx-fvs)
+    (type-fvo A A-fvs)
+    (bind-with-fvso A-fvs ctx-fvs A A-generalized)))
 
-(defne typing-state-o [gen gen-out ctx actx e B]
+
+;; ok. nom/hash works as expected
+(run 1 [q] (nom/fresh [x y]
+             (type-fvo [['v x] '-> [['v y] '-> ['v x]]] q)
+             (fresh [z zs]
+               (conso z zs q)
+               (nom/hash x zs))))
+
+;; ok. type properly generalized under the empty context
+(run 1 [q] (nom/fresh [x y]
+             (fresh [tvs]
+               (t-geno [] [['v x] '-> [['v y] '-> ['v x]]] q))))
+
+
+;; ok. type properly generalized when context is non-empty
+(run 1 [q] (nom/fresh [x y v]
+             (fresh [tvs]
+               (t-geno [[v ['v x]]] [['v x] '-> [['v y] '-> ['v x]]] q))))
+
+
+(defne typing-state-o [ctx actx e B]
   
   ;; t-int
   ([gen gen _ [] _ 'bool]
